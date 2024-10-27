0f43d3272e36024b56b30c555e10c36ed741dfef
Rosomak2024 <mbnet@o2.pl>
2024-10-27 13:02:58 +0100

    photos implement to site

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.pexels.com/v1/curated?page=2&per_page=10", {
+      headers: {
+        Authorization:
+          "N2Wa10zVQIclQiHpwbqdrNc5NypUMX9z9YYiv9alEcr8yuUKmhRwylJx", // Authorization header with the API key
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setPhotos(data.photos); // 'photos' key from the API response contains the array of photos
+        console.log("data", data);
+      })
+      .catch((error) => console.error("Error fetching the photos:", error));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {photos.length > 0 ? (
+        photos.map((photo) => (
+          <div className="photo_container" key={photo.id}>
+            <img
+              className="photo"
+              src={photo.src.medium}
+              // alt={photo.photographer}
+            ></img>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
-
-export default App
+export default App;
