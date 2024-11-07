73a7f9a1303b2f77897125e587a97314a73fad34
Rosomak2024 <mbnet@o2.pl>
2024-11-07 11:03:45 +0100

    button last if added

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,53 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-const[data,setData] = useState([]);
-const[loading,setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
-const fetchData = async() => {
-try {
-  const response = await fetch("https://jsonplaceholder.org/posts")  
-  const result = await response.json();
-   setData(result)
-   setLoading(false)
-   console.log(result)
-}catch(error) {setLoading(false) } }
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://swapi.dev/api/people?page=${page}`);
+      const result = await response.json();
+      setData(result.results);
+      console.log(result.results.slice(0, 5));
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setLoading(false);
+    }
+  };
 
-useEffect(()=>{
- fetchData();
-},[])
- 
-if(loading) return <p>Loading...</p>
- return(
-  <div>
-    <h1>
-    Fetched data
-    </h1>
-    <ul>
-      {data.map((user)=>{
-        return <li key={user.id}>{user.slug}<p>{user.url}</p></li>
-      })}
-    </ul>
-  </div>
- )
+  useEffect(() => {
+    // console.log();
+    fetchData();
+  }, [page]);
 
+  return (
+    <div>
+      {/* Loading Indicator */}
+      {loading && <p>Loading...</p>}
+      <h1>Star Wars</h1>
+      <ul>
+        {data.slice(0, 5).map((character) => (
+          <li
+            key={character.name}
+            style={{
+              border: "1px solid #ddd",
+              padding: "10px",
+              margin: "10px 0",
+            }}
+          >
+            {character.name}
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => setPage((prev) => prev + 1)}>Next Page</button>
+      {page > 1 && (
+        <button onClick={() => setPage((prev) => prev - 1)}>Last Page</button>
+      )}
+      {/* return <button onClick={() => setPage((prev) => prev - 1)}>Last Page</button> */}
+    </div>
+  );
 }
 export default App;
