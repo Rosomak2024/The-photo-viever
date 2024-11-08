f607b6c3d08b5c316117acb0caae2060a12b5aa1
Rosomak2024 <mbnet@o2.pl>
2024-11-08 10:30:37 +0100

    change the return handleclick and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-  // const [response, setResponse] = useState(null);
+  const [data, setData] = useState(null);
 
   const fetchData = async () => {
     try {
       const response = await fetch("https://dog.ceo/api/breeds/image/random/1");
-
-      const data = await response.data;
-      console.log("data", data);
-      setData(json);
+      const result = await response.json();
+      setData(result.message);
+      console.log(result.message);
       setLoading(false);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -22,27 +20,25 @@ function App() {
 
   useEffect(() => {
     // console.log();
-    fetchData(handleClick);
+    fetchData();
   }, []);
 
-  const handleClick = (e) => {
-    const value = e.target;
-    setForm(value);
+  const handleClick = () => {
+    fetchData();
   };
 
   return (
     <div>
-      {loading ? <p>Loading...</p> : <p>Data fetched successfully!</p>}
-      <h1>Post Fake API</h1>
-
-      <button onClick={handleClick}>Submit</button>
-
-      {response && (
+      <h1>Dog Api</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
         <div>
-          <h2>Response:</h2>
-          <img src="data.message"></img>
+          <h2>Dog image:</h2>
+          <img src={data} alt="Random dog"></img>
         </div>
       )}
+      <button onClick={handleClick}>Random</button>
     </div>
   );
 }
