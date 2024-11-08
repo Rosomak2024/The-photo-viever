c7e821ef65f580c655dc68b2b2c4ceea17825d2d
Rosomak2024 <mbnet@o2.pl>
2024-11-08 10:08:54 +0100

    first changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,16 @@ import "./App.css";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [form, setForm] = useState({ name: "", email: "" });
-  const [response, setResponse] = useState(null);
+  const [data, setData] = useState([]);
+  // const [response, setResponse] = useState(null);
 
   const fetchData = async () => {
     try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            name: form.name,
-            email: form.email,
-            userId: form.id,
-          }),
-          headers: {
-            "Content-type": "application/json; charset=UTF-8",
-          },
-        }
-      );
+      const response = await fetch("https://dog.ceo/api/breeds/image/random/1");
 
-      const json = await response.json();
-      console.log("json", json);
-      setResponse(json);
+      const data = await response.data;
+      console.log("data", data);
+      setData(json);
       setLoading(false);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -33,21 +20,14 @@ function App() {
     }
   };
 
-  // useEffect(() => {
-  //   // console.log();
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    // console.log();
+    fetchData(handleClick);
+  }, []);
 
-  const handleInputChange = (e) => {
-    const { id, value } = e.target;
-    setForm((prevForm) => ({
-      ...prevForm,
-      [id]: value,
-    }));
-  };
-
-  const handleSubmit = () => {
-    fetchData();
+  const handleClick = (e) => {
+    const value = e.target;
+    setForm(value);
   };
 
   return (
@@ -55,30 +35,12 @@ function App() {
       {loading ? <p>Loading...</p> : <p>Data fetched successfully!</p>}
       <h1>Post Fake API</h1>
 
-      <label htmlFor="name">Name: </label>
-      <input
-        autoComplete="on"
-        id="name"
-        value={form.name}
-        onChange={handleInputChange}
-      />
-
-      <label htmlFor="email">Email: </label>
-      <input
-        autoComplete="on"
-        id="email"
-        value={form.email}
-        onChange={handleInputChange}
-      />
-
-      <button onClick={handleSubmit}>Submit</button>
+      <button onClick={handleClick}>Submit</button>
 
       {response && (
         <div>
           <h2>Response:</h2>
-          <p>ID: {response.id}</p>
-          <p>Name: {response.name}</p>
-          <p>Email: {response.email}</p>
+          <img src="data.message"></img>
         </div>
       )}
     </div>
